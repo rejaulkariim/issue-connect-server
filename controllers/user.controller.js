7601c5f299d50b567bf65f40695c5ba236c1cc81
karimscoding <rejaulkarim66666@gmail.com>
2023-10-28 12:55:26 +0800

    added user login method and jwt

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const{createToken} = require("../helpers/token.helper")
 
 // Create user
 const createUser = async (req, res) => {
@@ -7,12 +8,29 @@ const createUser = async (req, res) => {
 
     const user = await User.signup(name, email, location, password);
 
-    res.status(200).json(user);
+    const token = createToken(user._id)
+
+    res.status(200).json({user, token});
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
-const loginUser = async (req, res) => {};
+
+// Login user
+const loginUser = async (req, res) => {
+  try {
+    const {email, password} = req.body
+
+    const user = await User.login(email, password)
+
+    const token = createToken(user._id)
+
+    res.status(200).json({user, token})
+
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   createUser,
---
+++ helpers/token.helper.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken")
+
+const createToken = id =>{
+    return jwt.sign({id}, process.env.JWT_TOKE,{expiresIn:"7d"})
+}
+
+module.exports ={createToken}
